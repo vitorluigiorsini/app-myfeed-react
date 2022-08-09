5539e5372cdb087eab595d2d7cfb82d2a99c8d2d
Vitor Orsini <vitorl85@gmail.com>
2022-08-09 20:15:10 -0300

    Atualiza local do index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import ReactDOM from "react-dom";
 
 import "./styles/global.css";
 
-import App from "./src/App";
+import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
